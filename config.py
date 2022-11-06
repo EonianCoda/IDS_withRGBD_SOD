0e80f0fab2c51942ecb53253ca104cbfb3ad8ff3
yunhsiang yang <textuseful159000@gmail.com>
2022-11-06 15:18:59 +0800

    first version

---
+++ config.py
@@ -0,0 +1,14 @@
+DATA = {
+    'data_root': "D:\\python\\RGBD\\data\\NJU2K",
+    'train_name_list': 'train_name_list.txt',
+    'val_name_list': 'val_name_list.txt',
+    'test_name_list': 'test_name_list.txt',
+}
+
+TRAIN = {
+    'GPU': "0",
+    'epoch': 200,
+    'learning_rate': 8e-5,
+    'batch_size': 8,
+    'num_superpixels': 100
+}
